07651cb449306908f02def9c9e7e3a10981d9e82
Lee Butler <butler1233@hotmail.com>
2017-03-21 10:29:42 +0000

    Nuked the old project
    It was such a piece of shit

--- ListingManager/Default.aspx.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class _Default
-    Inherits Page
-
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-        'SessionTest.Text = CType(Session("User"), WHLClasses.Employee).FullName
-    End Sub
-End Class
